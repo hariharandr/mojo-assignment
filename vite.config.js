999d70774b0c88b0ccbf51756e7701b149bf236e
hariharandr <hariravidhivya@gmail.com>
2025-11-19 03:24:46 +0530

    test-3:deployment dockerfile change for the 500 error fix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
-{
-  "scripts": {
-    "dev": "vite",
-    "build": "vite build",
-    "watch": "vite"
-  },
-  "devDependencies": {
-    "@tailwindcss/vite": "^4.1.17",
-    "laravel-vite-plugin": "^2.0.0",
-    "vite": "^7.0.7"
-  },
-  "overrides": {
-    "rollup": {
-      "rollup": "$rollup"
-    }
-  }
-}
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
